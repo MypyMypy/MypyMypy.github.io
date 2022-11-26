fd4720e95d0a3c762f7c0cde4ae9cd9596203e90
danich282@gmail.com <danich282@gmail.com>
2022-11-26 22:51:27 +0300

    fix startPage

--- scripts/startPage.js
+++ scripts/startPage.js
@@ -3,5 +3,6 @@
     const loader = document.querySelector('.loader');
     window.innerWidth >= window.innerHeight ?
       loader.style.width = '50vh' : loader.style.width = '50vw'
+    loader.style.height = loader.style.width;
   })
 })();
