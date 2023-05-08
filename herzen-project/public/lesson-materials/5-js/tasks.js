2e45c18605d393a08d9a9c1feb1734de417ee943
danich282@gmail.com <danich282@gmail.com>
2023-05-09 00:45:36 +0300

    herzen-project add public

---
+++ herzen-project/public/lesson-materials/5-js/tasks.js
@@ -0,0 +1,72 @@
+// Используйте объект Math и его методы в JavaScript, чтобы сгенерировать случайное число от 50 до 70 и округлить его до ближайшего целого числа.
+function math1 () {
+  // your code
+  return randomNumber
+}
+
+// Используйте объект Math в JavaScript для вычисления площади круга с учетом его радиуса, округленного до 2 знаков после запятой, используя формулу: S = π * r^2
+function math2 (radius) {
+  // your code
+  return square
+}
+/* Expected Result
+  console.log(math2(10)) // 314.16
+*/
+
+// Используйте строки JavaScript для реализации функции, которая принимает строку в качестве входных данных и заменяет все экземпляры данного слова другим словом. Функция должна быть нечувствительной к регистру, что означает, что она должна заменять слово независимо от того, появляется ли оно в строке в верхнем или нижнем регистре.
+function replaceWords(str, wordToReplace, replacementWord) {
+
+  return newString
+}
+/* Expected Result
+  console.log(replaceWords("The cat is black", "black", "white")); // "The cat is white"
+  console.log(replaceWords("The cat is Black", "black", "white")); // "The cat is white"
+*/
+
+//Используйте циклы JavaScript для реализации функции, которая принимает 2D-массив целых чисел и возвращает сумму всех элементов в массиве, которые делятся на 3.
+function sumDivisibleByThree (arr) {
+  // your code
+  return sum;
+};
+/* Expected Result
+  console.log(sumDivisibleByThree([[1, 2, 3], [4, 5, 6], [7, 8, 9]])); // 18
+*/
+
+// Используйте цикл JavaScript while для реализации функции, которая принимает положительное целое число n и вычисляет сумму первых n нечетных чисел
+function sumFirstNOddNumbers(n) {
+  // your code
+  return sum;
+};
+/* Expected Result
+  console.log(sumFirstNOddNumbers(5)); // 9
+*/
+
+// Используйте встроенные методы массива JavaScript для реализации функции, которая принимает массив объектов, представляющих список людей и их возраст, и возвращает имя самого старого человека.
+function findOldestPerson(peopleObj) {
+  // your code
+  return nameTheOldestPerson
+};
+/* Expected Result
+  console.log(
+    findOldestPerson([
+      { name: "John", age: 25 },
+      { name: "Jane", age: 32 },
+      { name: "Jim", age: 40 }
+    ])
+  ); // Jim
+*/
+
+// Используйте встроенные объектные методы JavaScript для реализации функции, которая принимает объект и возвращает новый объект, содержащий только свойства со значениями типа string.
+function onlyStringProperties(obj) {
+  // your code
+  return stringObject
+}
+/* Expected Result
+  console.log(
+    onlyStringProperties({
+      name: "John",
+      age: 25,
+      city: "London"
+    })
+  ); // { name: 'John', city: 'London' }
+*/
