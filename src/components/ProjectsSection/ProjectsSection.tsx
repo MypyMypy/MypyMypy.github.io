57c499501cd03ade0f6c960a67dccc928a25d159
mypymypy <danich282@gmail.com>
2024-02-20 01:31:14 +0300

    feat: add 404 listener for SPA GitPages

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -7,17 +7,14 @@ import { projects } from '../../data';
 import ProjectCard from '../ProjectCard/ProjectCard';
 import { useInView } from 'react-intersection-observer';
 
-
-
-
 const ProjectsSection: React.FC = () => {
   const [ref, inView] = useInView();
-  
+
   const slides = [];
   let projectsInSlide = [];
   let countProjectsInSlide = 1;
   const viewPortWidth = window.innerWidth;
-  if (viewPortWidth >= 768) countProjectsInSlide = 2; 
+  if (viewPortWidth >= 768) countProjectsInSlide = 2;
   if (viewPortWidth >= 1440) countProjectsInSlide = 3;
 
   for (let i = 0; i < projects.length; i++) {
