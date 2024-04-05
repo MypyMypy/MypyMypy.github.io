0ff8361665c378d53ba3a2ad976dbdc61e7b8877
mypymypy <danich282@gmail.com>
2024-04-05 19:35:05 +0300

    update: update data; feat: limited slides

--- src/widgets/sections/ui/ProjectsSection/ProjectsSection.tsx
+++ src/widgets/sections/ui/ProjectsSection/ProjectsSection.tsx
@@ -7,42 +7,43 @@ import 'swiper/scss';
 import { projects } from '@/shared/model/data';
 import { ProjectCard } from '@/entities/ProjectCard';
 
+const slides: React.ReactElement[] = [];
+let projectsInSlide = [];
+let countProjectsInSlide = 1;
+const viewPortWidth = window.innerWidth;
+if (viewPortWidth >= 768) countProjectsInSlide = 2;
+if (viewPortWidth >= 1440) countProjectsInSlide = 3;
+
+for (let i = 0; i < projects.length; i++) {
+  const projectData = projects[i];
+  const project = (
+    <ProjectCard
+      key={projectData.name}
+      name={projectData.name}
+      imagePath={projectData.image}
+      date={projectData.date}
+      description={projectData.description}
+      srcGit={projectData.srcGit}
+      tags={projectData.tags}
+      deploy={projectData.deploy}
+    />
+  );
+  projectsInSlide.push(project);
+  if (
+    projectsInSlide.length === countProjectsInSlide ||
+    (projectsInSlide.length && projects[i + 1] === undefined)
+  ) {
+    const slide = (
+      <SwiperSlide key={projectData.name}>{projectsInSlide}</SwiperSlide>
+    );
+    slides.push(slide);
+    projectsInSlide = [];
+    if (slides.length === 3) break;
+  }
+}
+
 export const ProjectsSection: React.FC = () => {
   const [ref, inView] = useInView();
-
-  const slides = [];
-  let projectsInSlide = [];
-  let countProjectsInSlide = 1;
-  const viewPortWidth = window.innerWidth;
-  if (viewPortWidth >= 768) countProjectsInSlide = 2;
-  if (viewPortWidth >= 1440) countProjectsInSlide = 3;
-
-  for (let i = 0; i < projects.length; i++) {
-    const projectData = projects[i];
-    const project = (
-      <ProjectCard
-        key={projectData.name}
-        name={projectData.name}
-        imagePath={projectData.image}
-        date={projectData.date}
-        description={projectData.description}
-        srcGit={projectData.srcGit}
-        tags={projectData.tags}
-        deploy={projectData.deploy}
-      />
-    );
-    projectsInSlide.push(project);
-    if (
-      projectsInSlide.length === countProjectsInSlide ||
-      (projectsInSlide.length && projects[i + 1] === undefined)
-    ) {
-      const slide = (
-        <SwiperSlide key={projectData.name}>{projectsInSlide}</SwiperSlide>
-      );
-      slides.push(slide);
-      projectsInSlide = [];
-    }
-  }
 
   return (
     <SectionWrapper
